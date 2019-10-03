5d181a603d1d75713da346ddd23f5f5f71fcd70a
INNOVECS\Roman.Mekhed <roman.mekhed@innovecs.com>
2019-10-03 11:35:09 +0300

    rmekhed css --> sass

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.sass';
 import Home from './home/Home';
+import Header from './header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header data="HOME"></Header>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Home data="HOME"></Home>
