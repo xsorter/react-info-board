9a350f65b937cd18f39defc7ba53df58aa76b6bf
xsorter <xsorter@gmail.com>
2019-11-02 09:45:04 +0000

    database url added

--- src/Api.js
+++ src/Api.js
@@ -1,7 +1,11 @@
+const PROJECT_ID = 'infoboard-react';
+
 const setData = () => {};
 
 const getData = async () => {
-  const response = await fetch('');
+  const response = await fetch(
+    `https://firestore.googleapis.com/v1/projects/${PROJECT_ID}/databases/(default)/documents/**COLLECTION_ID**?&key=(YOUR API KEY)`,
+  );
   const result = await result.json();
 };
 
