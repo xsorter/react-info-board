71c97a75c73235150cd4011e9e675edbc88e623b
xsorter <xsorter@gmail.com>
2019-11-04 18:16:26 +0200

    add getData config

--- src/Api.js
+++ src/Api.js
@@ -2,7 +2,36 @@ const PROJECT_ID = 'infoboard-react';
 const COLLECTION = 'retrospective';
 const DOCUMENT_ID = '1';
 
-const setData = () => {};
+const testItem = {
+  id: 43,
+  requestBody: {
+    fields: {
+      title: { stringValue: 'test data to POST' },
+      text: { stringValue: 'this is test text from POST request' },
+    },
+  },
+};
+
+const postConfig = {
+  method: 'POST',
+  cache: 'no-cache',
+  credentials: 'same-origin',
+  connection: 'keep-alive',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  referrer: 'no-referrer',
+  body: JSON.stringify(testItem.requestBody),
+};
+
+const setData = async () => {
+  const response = await fetch(
+    `https://firestore.googleapis.com/v1/projects/${PROJECT_ID}/databases/(default)/documents/${COLLECTION}?documentId=${testItem.id}`,
+    postConfig,
+  );
+  const result = await response.json();
+  return result;
+};
 
 const getData = async () => {
   const response = await fetch(
