5374c7e40d70b6667f2056b77715d31794fff308
Oleksii Nelin <xsorter@gmail.com>
2019-11-09 21:32:56 +0200

    id generator added

--- src/Helpers.js
+++ src/Helpers.js
@@ -1,5 +1,11 @@
 const helpers = {
-  idGenerator: () => {},
+  idGenerator: () => {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
+      let r = (Math.random() * 16) | 0,
+        v = c == 'x' ? r : (r & 0x3) | 0x8;
+      return v.toString(16);
+    });
+  },
 };
 
 export default helpers;
