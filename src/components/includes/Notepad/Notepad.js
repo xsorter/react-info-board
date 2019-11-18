25236763cc25961f7d9cb08077c6b62496f2c960
xsorter <xsorter@gmail.com>
2019-11-18 18:16:26 +0200

    styles added

--- src/components/includes/Notepad/Notepad.js
+++ src/components/includes/Notepad/Notepad.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
+import TextareaAutosize from '@material-ui/core/TextareaAutosize';
+import './Notepad.sass';
 
 class Notepad extends Component {
   render() {
-    return <div>Notepad</div>;
+    return (
+      <div className="notepad">
+        <div className="cBox">
+          <h2>Notepad</h2>
+          <TextareaAutosize className="notepad__textarea" aria-label="minimum height" rows={3} placeholder="Minimum 3 rows" />
+        </div>
+      </div>
+    );
   }
 }
 
