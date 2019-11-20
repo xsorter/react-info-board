4ed615838bf277902a7d63191d6f4a231423faea
Oleksii Nelin <xsorter@gmail.com>
2019-11-20 23:17:51 +0200

    text redactor added

--- src/components/includes/Notepad/Notepad.js
+++ src/components/includes/Notepad/Notepad.js
@@ -1,25 +1,47 @@
 import React, { Component } from 'react';
-import TextareaAutosize from '@material-ui/core/TextareaAutosize';
 import './Notepad.sass';
 import Button from '@material-ui/core/Button';
+import { Editor } from 'react-draft-wysiwyg';
+import { EditorState } from 'draft-js';
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 
 class Notepad extends Component {
+  constructor() {
+    super();
+    this.state = {
+      editorState: EditorState.createEmpty(),
+    };
+  }
+
+  contentChangeHandler = editorState => {
+    this.setState({
+      editorState,
+    });
+  };
+  saveHandler = () => {
+    console.log('SAVED');
+  };
+  clearHandler = () => {
+    console.log('CLEAR');
+  };
   render() {
+    const { editorState } = this.state;
     return (
       <div className="notepad">
         <div className="cBox">
           <h2 className="notepad__title">Notepad</h2>
-          <TextareaAutosize
-            className="notepad__textarea"
-            aria-label="minimum height"
-            rows={3}
-            placeholder="Type text here"
+          <Editor
+            editorState={editorState}
+            toolbarClassName="notepad__toolbar"
+            wrapperClassName="notepad__wrapper"
+            editorClassName="notepad__textarea"
+            onEditorStateChange={this.contentChangeHandler}
           />
           <div className="notepad__buttons">
-            <Button variant="outlined" size="medium" color="primary">
+            <Button onClick={this.saveHandler} variant="outlined" size="medium" color="primary">
               Save
             </Button>
-            <Button variant="outlined" size="medium" color="secondary">
+            <Button onClick={this.clearHandler} variant="outlined" size="medium" color="secondary">
               Clear
             </Button>
           </div>
