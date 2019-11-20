9c37ae9de4d344d0209c430ee2c73ca505b40b3e
Oleksii Nelin <xsorter@gmail.com>
2019-11-20 22:33:17 +0200

    notepad styles added

--- src/components/includes/Notepad/Notepad.js
+++ src/components/includes/Notepad/Notepad.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 import TextareaAutosize from '@material-ui/core/TextareaAutosize';
 import './Notepad.sass';
+import Button from '@material-ui/core/Button';
 
 class Notepad extends Component {
   render() {
     return (
       <div className="notepad">
         <div className="cBox">
-          <h2>Notepad</h2>
-          <TextareaAutosize className="notepad__textarea" aria-label="minimum height" rows={3} placeholder="Minimum 3 rows" />
+          <h2 className="notepad__title">Notepad</h2>
+          <TextareaAutosize
+            className="notepad__textarea"
+            aria-label="minimum height"
+            rows={3}
+            placeholder="Type text here"
+          />
+          <div className="notepad__buttons">
+            <Button variant="outlined" size="medium" color="primary">
+              Save
+            </Button>
+            <Button variant="outlined" size="medium" color="secondary">
+              Clear
+            </Button>
+          </div>
         </div>
       </div>
     );
