1f524777a88f0b2202f211e00f98bda3cc3cca5d
xsorter <xsorter@gmail.com>
2019-11-29 17:58:33 +0200

    added noty component

---
+++ src/components/hoc/Noty.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const notyContainer = WrappedComponent => {
+  return class extends React.Component {
+    constructor(props){
+      super(props);
+
+    }
+  }
+}
+
+export default notyContainer;
