b1df25544bf6ab734614fb3a9bf5b3f10a64a479
Oleksii Nelin <xsorter@gmail.com>
2019-12-04 00:19:51 +0200

    added message state

--- src/components/hoc/Noty.js
+++ src/components/hoc/Noty.js
@@ -7,36 +7,37 @@ const notyContainer = (WrappedComponent, data) => {
       super(props);
 
       this.state = {
-        type: 'success',
+        type: 'info',
         show: false,
-        message: ''
-      }
+        message: '',
+      };
 
       console.log('HOC DATA', data);
       console.log('PROPS', this.props);
       this.data = data;
     }
 
-    messageHandler = (message) => {
-      console.log('MESSAGE', message)
-    }
+    messageHandler = message => {
+      const newMessage = { ...message };
+      this.setState(newMessage);
+    };
 
     render() {
       const noty = this.state;
       console.log(noty);
       return (
         <div>
-          {noty.show ?
-          <div className="noty noty__success">
-            <div className="noty__message">
-              {noty.message}
+          {noty.show ? (
+            <div className={`noty noty__${noty.type}`}>
+              <div className="noty__message">{noty.message}</div>
+              <div className="noty__close">
+                <span>&#x2716;</span>
+              </div>
             </div>
-            <div className="noty__close">
-              <span>&#x2716;</span>
-            </div>
-          </div>
-          : null}
-          <WrappedComponent onMessageFired={(message) => this.messageHandler(message)} ></WrappedComponent>
+          ) : null}
+          <WrappedComponent
+            onMessageFired={message => this.messageHandler(message)}
+          ></WrappedComponent>
         </div>
       );
     }
