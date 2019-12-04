f40bbe8b2df7310f620e597c79619049d7122c77
xsorter <xsorter@gmail.com>
2019-12-04 17:52:01 +0200

    code refactoring

--- src/components/hoc/Noty.js
+++ src/components/hoc/Noty.js
@@ -2,48 +2,34 @@ import React from 'react';
 import './Noty.sass';
 import { CSSTransition } from 'react-transition-group';
 
-const notyContainer = (WrappedComponent, data) => {
+const notyContainer = (WrappedComponent) => {
   return class extends React.Component {
     constructor(props) {
       super(props);
-
       this.state = {
         type: 'info',
         show: false,
         message: '',
       };
-
-      console.log('HOC DATA', data);
-      console.log('PROPS', this.props);
-      this.data = data;
     }
 
     messageHandler = message => {
       const newMessage = { ...message };
       this.setState(newMessage);
-
       setTimeout(() => {
-        this.setState({
-          type: 'general',
-          show: false,
-          message: '',
-        })
+        this.setState({show: false})
       }, 5000)
     };
 
     closeHandler = () =>{
-      this.setState({
-        type: 'general',
-        show: false,
-        message: '',
-      });
+      this.setState({show: false});
     }
+
     render() {
       const noty = this.state;
       console.log(noty);
       return (
         <div>
-
           <CSSTransition unmountOnExit in={noty.show} timeout={800} classNames="noty-transitions">
             <div className={`noty noty__${noty.type}`}>
               <div className="noty__message">{noty.message}</div>
@@ -52,7 +38,6 @@ const notyContainer = (WrappedComponent, data) => {
               </div>
             </div>
           </CSSTransition >
-
           <WrappedComponent
             onMessageFired={message => this.messageHandler(message)}
           ></WrappedComponent>
