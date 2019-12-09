c07ce8f8ccceaee502b604c9a9ba81edc89cbc3f
xsorter <xsorter@gmail.com>
2019-12-09 16:30:07 +0200

    moved menu to separate component

--- src/components/includes/Header/Header.js
+++ src/components/includes/Header/Header.js
@@ -1,25 +1,10 @@
 import React from 'react';
 import headerLogoPng from './logo.png';
 import './Header.sass';
-import Button from '@material-ui/core/Button';
-import MenuIcon from '@material-ui/icons/Menu';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
-import { Home, PostAdd, Archive } from '@material-ui/icons';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
 import { Link } from 'react-router-dom';
+import HeaderMenu from './HeaderMenu/HeaderMenu';
 
 const Header = () => {
-  const [anchorEl, setAnchorEl] = React.useState(null);
-
-  const handleClick = event => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
   return (
     <div className="Header">
       <div className="cBox">
@@ -30,46 +15,7 @@ const Header = () => {
           </Link>
         </div>
         <div className="Header-col">
-          <Button
-            size="large"
-            color="secondary"
-            className="Header-menuTrigger"
-            aria-controls="header-menu"
-            aria-haspopup="true"
-            onClick={handleClick}
-          >
-            <MenuIcon />
-          </Button>
-          <Menu
-            id="header-menu"
-            anchorEl={anchorEl}
-            getContentAnchorEl={null}
-            anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-            transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-            keepMounted
-            open={Boolean(anchorEl)}
-            onClose={handleClose}
-            className="Header-menuList"
-          >
-            <MenuItem onClick={handleClose} exact to="/" component={Link}>
-              <ListItemIcon>
-                <Home fontSize="small" />
-              </ListItemIcon>
-              <ListItemText primary="Homepage" />
-            </MenuItem>
-            <MenuItem onClick={handleClose} to="/add-new" component={Link}>
-              <ListItemIcon>
-                <PostAdd fontSize="small" />
-              </ListItemIcon>
-              <ListItemText primary="Add new issue" />
-            </MenuItem>
-            <MenuItem onClick={handleClose} to="/archive" component={Link}>
-              <ListItemIcon>
-                <Archive fontSize="small" />
-              </ListItemIcon>
-              <ListItemText primary="Archive" />
-            </MenuItem>
-          </Menu>
+          <HeaderMenu />
         </div>
       </div>
     </div>
