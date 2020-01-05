30ba8e79be5a87f2e07945da082358bca4bb0e99
Aleksey Nelin <xsorter@gmail.com>
2020-01-05 18:54:08 +0300

    added responsible person select

--- src/components/includes/Settings/Settings.js
+++ src/components/includes/Settings/Settings.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import './Settings.sass';
 import Paper from '@material-ui/core/Paper';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
 
 class Settings extends React.Component {
   constructor(props){
     super(props)
 
+    this.state = {
+      labelWidth: 0,
+    };
+
+    this.inputLabel = React.createRef();
+
+  }
+
+  componentDidMount() {
+    this.setState({
+      labelWidth: this.inputLabel.current.offsetWidth,
+    });
   }
 
 
@@ -15,7 +31,22 @@ class Settings extends React.Component {
         <div className="cBox">
           <h2 className="settings__title">Settings</h2>
           <Paper className="settings__box">
-            TEST
+            <div className="settings__responsible">
+            <FormControl variant="outlined" fullWidth>
+              <InputLabel ref={this.inputLabel} id="outlined-label">
+                Responsible Employee
+              </InputLabel>
+              <Select
+                labelId="outlined-label"
+                labelWidth={this.state.labelWidth}
+                value=""
+              >
+                <MenuItem value="">
+                  Mekhed
+                </MenuItem>
+              </Select>
+            </FormControl>
+            </div> 
           </Paper>
         </div>
       </div>
