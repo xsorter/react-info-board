18cea5b80c47b1f7cde64c08a7a56a11d184898e
xsorter <xsorter@gmail.com>
2020-01-10 18:21:01 +0200

    button added

--- src/components/includes/Settings/Settings.js
+++ src/components/includes/Settings/Settings.js
@@ -5,6 +5,7 @@ import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
+import Button from '@material-ui/core/Button';
 import Api from '../../../Api';
 
 class Settings extends React.Component {
@@ -53,25 +54,33 @@ class Settings extends React.Component {
           <h2 className="settings__title">Settings</h2>
           <Paper className="settings__box">
             <div className="settings__responsible">
-              <FormControl variant="outlined" fullWidth>
-                <InputLabel ref={this.inputLabel} id="outlined-label">
-                  Responsible Employee
-                </InputLabel>
-                <Select
-                  onChange={this.handleSelectChange}
-                  value={this.state.responsibleEmployee}
-                  labelId="outlined-label"
-                  labelWidth={this.state.labelWidth}
-                >
-                  {users.map((e, i) => {
-                    return (
-                      <MenuItem key={i} value={e.shortName.stringValue}>
-                        {e.fullName.stringValue}
-                      </MenuItem>
-                    );
-                  })}
-                </Select>
-              </FormControl>
+              <div className="settings__responsible-box">
+                <FormControl variant="outlined" fullWidth>
+                  <InputLabel ref={this.inputLabel} id="outlined-label">
+                    Responsible Employee
+                  </InputLabel>
+                  <Select
+                    onChange={this.handleSelectChange}
+                    value={this.state.responsibleEmployee}
+                    labelId="outlined-label"
+                    labelWidth={this.state.labelWidth}
+                  >
+                    {users.map((e, i) => {
+                      return (
+                        <MenuItem key={i} value={e.shortName.stringValue}>
+                          {e.fullName.stringValue}
+                        </MenuItem>
+                      );
+                    })}
+                  </Select>
+                </FormControl>
+              </div>
+
+              <div className="settings__responsible-button">
+                  <Button disabled={!this.state.responsibleEmployee} size="large" type="submit" variant="contained" color="primary" fullWidth>
+                      Save
+                  </Button>
+              </div>
             </div>
           </Paper>
         </div>
